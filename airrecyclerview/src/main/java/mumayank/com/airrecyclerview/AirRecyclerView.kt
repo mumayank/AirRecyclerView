1fedf8a876ba2ff17d1fe54f2621ff33c3d6803a
mu <mumayank@gmail.com>
2018-11-29 16:22:22 +0530

    update

---
+++ airrecyclerview/src/main/java/mumayank/com/airrecyclerview/AirRecyclerView.kt
@@ -0,0 +1,89 @@
+package mumayank.com.airrecyclerview
+
+import android.app.Activity
+import android.support.v7.widget.GridLayoutManager
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
+import android.support.v7.widget.StaggeredGridLayoutManager
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.LinearLayout
+
+class AirRecyclerView {
+
+    interface AirRecyclerViewCallbacks {
+        fun getViewType(position: Int): Int
+        fun getViewLayout(viewType: Int): Int
+        fun getViewHolder(view: View, viewType: Int): RecyclerView.ViewHolder
+        fun getBindView(viewHolder: RecyclerView.ViewHolder, viewType: Int, position: Int)
+        fun getSize(): Int
+    }
+
+    companion object {
+
+        fun initAndGetAdapter(
+            activity: Activity,
+            recyclerView: RecyclerView,
+            airRecyclerViewCallbacks: AirRecyclerViewCallbacks,
+            layoutManager: RecyclerView.LayoutManager = LinearLayoutManager(activity)
+        ): RecyclerView.Adapter<RecyclerView.ViewHolder>
+        {
+            val rvAdapter = object : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+
+                override fun getItemViewType(position: Int): Int {
+                    return airRecyclerViewCallbacks.getViewType(position)
+                }
+
+                override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+                    val view = LayoutInflater.from(activity).inflate(airRecyclerViewCallbacks.getViewLayout(viewType), parent, false)
+                    return airRecyclerViewCallbacks.getViewHolder(view, viewType)
+                }
+
+                override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+                    airRecyclerViewCallbacks.getBindView(holder, airRecyclerViewCallbacks.getViewType(position), position)
+                }
+
+                override fun getItemCount(): Int {
+                    return airRecyclerViewCallbacks.getSize()
+                }
+
+            }
+
+            recyclerView.layoutManager = layoutManager
+            recyclerView.adapter = rvAdapter
+            return rvAdapter
+        }
+
+    }
+
+}
+
+
+/*
+Examples:
+ */
+
+
+/*
+View holder class:
+
+class CustomViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+    val parentLayout = view.parentLayout
+    val textView = view.textView
+}
+ */
+
+
+
+/*
+Layout managers:
+
+LinearLayoutManager(activity)
+LinearLayoutManager(activity, LinearLayoutManager.HORIZONTAL, false)
+LinearLayoutManager(activity, LinearLayoutManager.HORIZONTAL, true)
+GridLayoutManager(activity, spanCount)GridLayoutManager(activity, spanCount, LinearLayoutManager.HORIZONTAL, false)
+GridLayoutManager(activity, spanCount, LinearLayoutManager.HORIZONTAL, true)
+StaggeredGridLayoutManager(spanCount, StaggeredGridLayoutManager.VERTICAL)
+StaggeredGridLayoutManager(spanCount, StaggeredGridLayoutManager.HORIZONTAL)
+ */
--- app/src/main/java/mumayank/com/airrecyclerviewproject/MainActivity.kt
+++ app/src/main/java/mumayank/com/airrecyclerviewproject/MainActivity.kt
@@ -1,12 +1,84 @@
 package mumayank.com.airrecyclerviewproject
 
+import android.app.Activity
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
+import android.support.v7.widget.StaggeredGridLayoutManager
+import android.view.View
+import android.widget.LinearLayout
+import android.widget.Toast
+import kotlinx.android.synthetic.main.item.view.*
+import mumayank.com.airrecyclerview.AirRecyclerView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val strings = ArrayList<String>()
+        for (i in 1..100) {
+            strings.add(i.toString())
+        }
+
+        val VIEW_TYPE_1 = 0
+        val VIEW_TYPE_2 = 1
+
+        val rvAdapter = AirRecyclerView.initAndGetAdapter(
+            this,
+            findViewById(R.id.rv),
+            object: AirRecyclerView.AirRecyclerViewCallbacks {
+                override fun getViewType(position: Int): Int {
+                    return 0
+
+                    /*
+                    can also make use of multiple view types like this:
+
+                    if (position%3 == 0) {
+                        return VIEW_TYPE_1
+                    } else {
+                        return VIEW_TYPE_2
+                    }
+                     */
+                }
+
+                override fun getViewLayout(viewType: Int): Int {
+                    return R.layout.item
+
+                    // can also make use of viewType
+                }
+
+                override fun getViewHolder(view: View, viewType: Int): RecyclerView.ViewHolder {
+                    return CustomViewHolder(view)
+
+                    // can also make use of viewType
+                }
+
+                override fun getBindView(viewHolder: RecyclerView.ViewHolder, viewType: Int, position: Int) {
+                    val customViewHolder = viewHolder as CustomViewHolder
+                    val string = strings[position]
+
+                    customViewHolder.textView.text = string
+
+                    // can also make use of viewType
+                }
+
+                override fun getSize(): Int {
+                    return strings.size
+                }
+
+            },
+            StaggeredGridLayoutManager(5, StaggeredGridLayoutManager.VERTICAL) // optional, by default LinearLayoutManager will be used
+        )
+
+        // rvAdapter can be used later to perform notifyDataSetChanged()
+
+    }
+
+    class CustomViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val parentLayout = view.parentLayout
+        val textView = view.textView
     }
 }
