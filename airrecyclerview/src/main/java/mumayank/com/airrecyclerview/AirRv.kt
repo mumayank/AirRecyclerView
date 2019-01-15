62393c37f59afe1e6ae848140165dfe54ea22bf8
mu <mayank@einsite.io>
2019-01-15 21:36:52 +0530

    update

--- airrecyclerview/src/main/java/mumayank/com/airrecyclerview/AirRv.kt
+++ airrecyclerview/src/main/java/mumayank/com/airrecyclerview/AirRv.kt
@@ -15,39 +15,39 @@ class AirRv(
     val rv: RecyclerView
 
     interface Callback {
-        fun getAppContext(): Context
-        fun getParentLayoutViewGroup(): ViewGroup
-        fun getLayoutManager(): RecyclerView.LayoutManager
-        fun getViewType(position: Int): Int
-        fun getViewLayout(viewType: Int): Int
+        fun getAppContext(): Context?
+        fun getParentLayoutViewGroup(): ViewGroup?
+        fun getLayoutManager(): RecyclerView.LayoutManager?
+        fun getViewType(position: Int): Int?
+        fun getViewLayout(viewType: Int): Int?
         fun getViewHolder(view: View, viewType: Int): RecyclerView.ViewHolder
         fun getBindView(viewHolder: RecyclerView.ViewHolder, viewType: Int, position: Int)
-        fun getSize(): Int
+        fun getSize(): Int?
     }
 
     init {
         rvAdapter = object : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
             override fun getItemViewType(position: Int): Int {
-                return callback.getViewType(position)
+                return callback.getViewType(position) ?: 0
             }
 
             override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
-                return callback.getViewHolder(LayoutInflater.from(callback.getAppContext()).inflate(callback.getViewLayout(viewType), parent, false), viewType)
+                return callback.getViewHolder(LayoutInflater.from(callback.getAppContext()).inflate(callback.getViewLayout(viewType) ?: 0, parent, false), viewType)
             }
 
             override fun onBindViewHolder(viewHolder: RecyclerView.ViewHolder, position: Int) {
-                return callback.getBindView(viewHolder, callback.getViewType(position), position)
+                return callback.getBindView(viewHolder, callback.getViewType(position) ?: 0, position)
             }
 
             override fun getItemCount(): Int {
-                return callback.getSize()
+                return callback.getSize() ?: 0
             }
 
         }
 
         val rvView = LayoutInflater.from(callback.getAppContext()).inflate(R.layout.rv, callback.getParentLayoutViewGroup(), false)
-        callback.getParentLayoutViewGroup().addView(rvView)
+        callback.getParentLayoutViewGroup()?.addView(rvView)
         rv = rvView.findViewById(R.id.rv)
         rv.layoutManager = callback.getLayoutManager()
         rv.adapter = rvAdapter
